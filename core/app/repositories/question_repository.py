869cfde08467eef1d775edc5ac55753e91b95f8a
bloodeRok <spoluyakhtov99@gmail.com>
2023-10-11 13:21:53 +0300

    feat(docker): Added docker files.

--- core/app/repositories/question_repository.py
+++ core/app/repositories/question_repository.py
@@ -6,30 +6,6 @@ from core.models import Question
 class QuestionRepository:
     model = Question
 
-    def create(
-            self,
-            external_id: int,
-            text: str,
-            answer: str,
-            value: int,
-            created_at: str,
-            updated_at: str
-    ) -> Question:
-        """
-        Creates a new Question object in the database.
-        """
-
-        new_question = self.model.objects.create(
-            id=external_id,
-            text=text,
-            answer=answer,
-            value=value,
-            created_at=created_at,
-            updated_at=updated_at
-        )
-
-        return new_question
-
     def bulk_create(
             self,
             questions: list[dict[str, Any]]
@@ -46,26 +22,27 @@ class QuestionRepository:
 
         Example usage:
             questions_data = [
-                {'id': 1, 'question': 'What is...?', 'answer': 'The answer', 'value': 5, ...},
-                {'id': 2, 'question': 'How does...?', 'answer': 'Another answer', 'value': 10, ...},
+                {'id': 1, 'question': 'What is...?', 'answer': 'The answer', 'value': 5, ...}, \n
+                {'id': 2, 'question': 'How ...?', 'answer': 'Another answer', 'value': 10, ...}, \n
                 ...
-            ]
+            ] \n
             new_questions = bulk_create(questions_data)
         """
 
-        new_questions = []
-        for question in questions:
-            new_question = self.create(
-                external_id=question["id"],
-                text=question["question"],
-                answer=question["answer"],
-                value=question["value"],
-                created_at=question["created_at"],
-                updated_at=question["updated_at"]
-            )
-            new_questions.append(new_question)
+        return self.model.objects.bulk_create(
+            [
+                Question(
+                    id=question["id"],
+                    text=question["question"],
+                    answer=question["answer"],
+                    value=question["value"],
+                    created_at=question["created_at"],
+                    updated_at=question["updated_at"]
+                )
+                for question in questions
 
-        return new_questions
+            ]
+        )
 
     def get_all_ids(self) -> list[int]:
         """
--- server/settings.py
+++ server/settings.py
@@ -11,9 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
-from dotenv import load_dotenv
-
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -112,8 +109,8 @@ DATABASES = {
         "NAME": os.environ.get("POSTGRES_DB", ""),
         "USER": os.environ.get("POSTGRES_USER", ""),
         "PASSWORD": os.environ.get("POSTGRES_PASSWORD", ""),
-        "HOST": os.environ.get("POSTGRES_HOST", ""),
-        "PORT": os.environ.get("POSTGRES_PORT", ""),
+        "HOST": os.environ.get("DATABASE_HOST", ""),
+        "PORT": os.environ.get("DATABASE_PORT", ""),
     }
 }
 
